5afd813cd2dd75043172ae232c85031d94a2bad1
Ranvir Choudhary <ranvir2002choudhary@gmail.com>
2022-05-03 15:22:16 +0530

    let's goo now pressing a key, makes the code test if it's the correct key. there's still some bugs (check code for comment about bug) but it's great because you did it all by yourself!:D no help. good job

--- Play/main.js
+++ Play/main.js
@@ -4,7 +4,6 @@ async function extractData(random){
   const response = await fetch("./words.txt");
   const data = await response.text();
   const words = data.split("\r\n");
-  console.log(words[random].split(""));
   WordToGuess = words[random].split("")
 }
 
@@ -13,32 +12,20 @@ function GenRandom(multiplier){
   return random;
 }
 
-// this contains() function is made so you can run the <arrayname>.some()
-// function. but you need to debug code first to run this.
-// read comment below to understand
-function contains(event, array){
-  for(i = 0; i === array.length; i++){
-    event === array[i]
-  }
-}
-
-// Turns out that you cant run this code 
-// because whenever you call the array returned by
-// extractData outside of it, the array still hasn't
-// loaded yet, because extractData is an async function
-// so you need to learn and understand async functions first,
-// then you can get to debugging this code. 
-// to recap, the problem is that you are trying to call the 
-// array before it has been loaded by the async function extractData, so you
-// "async" the calling of the function extractData to fix this problem, this line was paraphrased
-// from what @Xetera said on the discord server "The Programmer's Hangout" on 1/5/22
-
-
 function InputHandler(){
-  if(Array.isArray(WordToGuess)){console.log("is array")}
-    else {console.log("even bigger wtf")}
+  console.log(WordToGuess)
+  window.addEventListener("keypress", e => {
+    WordToGuess.forEach(element =>  {
+      if(element == e.key){
+        console.log("correct key!");
+      }
+      else{                         //this code executes every 8 times, because
+        console.log("wrong key!"); //you are using forEach for every element of the array so
+      }                           //every element that is wrong, the code is executed. this must be fixed
+    })                           //but right now, it's time for a break
+  })
 }
 
 extractData(GenRandom(276))
   .then(InputHandler)
-  .catch(() => console.log("err"))
+  .catch(() => console.log())
