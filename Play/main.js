34cc091423ba6af7dc558c0598dcd0709b8cd56d
Ranvir Choudhary <ranvir2002choudhary@gmail.com>
2022-05-06 10:58:50 +0530

    now pressing the right key inserts all the letters correctly! finally! it's done. now only the incorrect guess and hangman image changing features are left. good job.

--- Play/main.js
+++ Play/main.js
@@ -1,5 +1,6 @@
 let WordToGuess;
-const wordsGuessed = document.querySelectorAll(".letter")
+const wordsGuessed = document.querySelectorAll(".letter");
+const guessedWrong = document.querySelectorAll(".incorrect-guess");
 
 async function extractData(random) {
   const response = await fetch("./words.txt");
@@ -16,19 +17,32 @@ function GenRandom(multiplier) {
 function InputHandler() {
   console.log(WordToGuess);
   window.addEventListener("keypress", (e) => {
-    WordToGuess.forEach(element => {
-      if (element === e.key) {
-        console.log(WordToGuess.indexOf(element))
-        // console.log("hahaha")
+    let templateGuessed = [];
+    for (let letter of WordToGuess) {
+      if (letter === e.key) {
+        templateGuessed.push(true);
+        console.log(templateGuessed);
         // wordsGuessed[WordToGuess.indexOf(e.key)].textContent = e.key
-      } else {                                 //this code executes every 8 times, because
-        console.log("wrong key!");           //you are using forEach for every element of the array so
-      }                                    //every element that is wrong, the code is executed. this must be fixed
-    });                                  //but right now, it's time for a break
-  }); 
+      } else {
+        //this code executes every 8 times, because
+        //you are using forEach for every element of the array so
+        //every element that is wrong, the code is executed. this must be fixed
+        // turns out this code being executed more than one time proved to be in your favour lol
+        templateGuessed.push(false);
+        console.log(templateGuessed);
+      }
+    }
+    for (let i = 0; i < templateGuessed.length; i++) {
+      const truth = templateGuessed[i];
+      if(truth === true){
+        wordsGuessed[i].textContent = e.key
+        console.log("hello")
+      }
+    }
+  });
 }
 
-console.log(wordsGuessed)
+console.log(wordsGuessed);
 
 extractData(GenRandom(276))
   .then(InputHandler)
